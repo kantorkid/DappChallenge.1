e13f5563f089419017f5b07b5c111858ca5e5222
kantorkid <yourgitemail@example.com>
2023-06-04 01:23:27 -0400

    3rd commit

--- contracts/YieldAggregator.sol
+++ contracts/YieldAggregator.sol
@@ -1,8 +1,10 @@
 // Import necessary dependencies and interfaces
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import { Pool } from "aave-v3-core/contracts/protocol/pool/Pool.sol";
 
 contract YieldAggregator {
     address public compoundContract; // Address of Compound V3 contract
-    address public aaveContract; // Address of Aave V3 contract
+    address public aaveContract = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9; // Address of Aave V3 contract 
     
     // Other state variables and mappings
     
@@ -47,6 +49,99 @@ contract YieldAggregator {
         // Withdraw funds from the selected platform
         
         // Update necessary state variables
+        
+        // Emit event
+    }
+    
+    // Other helper functions and event declarations
+}
+
+
+
+
+
+
+// Import necessary dependencies and interfaces
+import { Pool } from "aave-v3-core/contracts/protocol/pool/Pool.sol";
+
+contract YieldAggregator {
+    address public WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // Address of WETH contract
+    address public cWETH = 0xA17581A9E3356d9A858b789D68B4d866e593aE94; // Address of cWETH contract, Compund's placeholder token
+    address public aaveContract = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9; // Address of Aave V3 contract 
+    
+    // Other state variables and mappings
+    
+    constructor(address _compoundContract, address _aaveContract) {
+        compoundContract = _compoundContract;
+        aaveContract = _aaveContract;
+    }
+    
+    // Deposit user funds into either Compound or Aave depending on higher APY
+    function deposit(uint256 amount) external {
+        // Calculate APY for Compound and Aave
+        uint256 compoundAPY = getCompoundAPY();
+        uint256 aaveAPY = getAaveAPY();
+        
+        // Determine which platform has a higher APY
+        bool compoundHasHigherAPY = compoundAPY > aaveAPY;
+        
+        // Deposit funds into the platform with a higher APY
+        if (compoundHasHigherAPY) {
+            // Deposit into Compound
+            Compound.deposit(amount);
+            
+            // Update necessary state variables
+        } else {
+            // Deposit into Aave
+            Aave.deposit(amount);
+            
+            // Update necessary state variables
+        }
+        
+        // Emit event
+    }
+    
+    // Withdraw funds from one platform to another if the APY is higher on the other platform
+    function rebalance() external {
+        // Check if rebalancing is necessary (based on APY)
+        uint256 compoundAPY = getCompoundAPY();
+        uint256 aaveAPY = getAaveAPY();
+        
+        bool compoundHasHigherAPY = compoundAPY > aaveAPY;
+        
+        if (compoundHasHigherAPY) {
+            // Withdraw from Aave and deposit into Compound
+            Aave.withdraw(amount);
+            Compound.deposit(amount);
+            
+            // Update necessary state variables
+        } else {
+            // Withdraw from Compound and deposit into Aave
+            Compound.withdraw(amount);
+            Aave.deposit(amount);
+            
+            // Update necessary state variables
+        }
+        
+        // Emit event
+    }
+    
+    // Withdraw user funds from either Compound or Aave and return to their account
+    function withdraw(uint256 amount) external {
+        // Check from which platform the user wants to withdraw
+        bool withdrawFromCompound = // Logic to determine if user wants to withdraw from Compound
+        
+        if (withdrawFromCompound) {
+            // Withdraw from Compound
+            Compound.withdraw(amount);
+            
+            // Update necessary state variables
+        } else {
+            // Withdraw from Aave
+            Aave.withdraw(amount);
+            
+            // Update necessary state variables
+        }
         
         // Emit event
     }
