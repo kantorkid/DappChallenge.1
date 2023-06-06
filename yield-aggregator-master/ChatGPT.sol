2e95e10e09c8619f38113fcbd914ef31c492e719
kantorkid <yourgitemail@example.com>
2023-06-06 00:58:22 -0400

    fixed folder

--- yield-aggregator-master/ChatGPT.sol
+++ yield-aggregator-master/ChatGPT.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.5.16;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
-interface WrappedETH {
+// Interface for ERC20 WETH contract
+interface WETH {
     function deposit() external payable;
     function withdraw(uint256) external;
     function approve(address, uint256) external returns (bool);
@@ -12,13 +13,15 @@ interface WrappedETH {
     function balanceOf(address) external view returns (uint256);
 }
 
-interface CompoundETH {
+// Interface for Compound's cETH contract
+interface cETH {
     function mint() external payable;
     function redeem(uint256) external returns (uint256);
     function supplyRatePerBlock() external view returns (uint256);
     function balanceOf(address) external view returns (uint256);
 }
 
+// Interface for Aave's lending pool contract
 interface AaveLendingPool {
     function deposit(
         address asset,
@@ -47,51 +50,62 @@ interface AaveLendingPool {
     );
 }
 
-contract YieldAggregator {
+contract Aggregator {
     using SafeMath for uint256;
 
+    // Variables
     string public name = "Yield Aggregator";
     address public owner;
-    address public locationOfFunds;
+    address public locationOfFunds; // Keep track of where the user balance is stored
     uint256 public amountDeposited;
 
-    WrappedETH private wrappedETH;
-    CompoundETH private compoundETH;
-    AaveLendingPool private aaveLendingPool;
+    WETH weth;
+    cETH cEth;
+    AaveLendingPool aaveLendingPool;
 
+    // Events
     event Deposit(address owner, uint256 amount, address depositTo);
     event Withdraw(address owner, uint256 amount, address withdrawFrom);
     event Rebalance(address owner, uint256 amount, address depositTo);
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner");
+        require(msg.sender == owner);
         _;
     }
 
-    constructor(address wrappedETHAddress, address compoundETHAddress, address aaveLendingPoolAddress) public {
+    // Constructor
+    constructor(address wethAddress, address cEthAddress, address aaveLendingPoolAddress) public {
         owner = msg.sender;
-        wrappedETH = WrappedETH(wrappedETHAddress);
-        compoundETH = CompoundETH(compoundETHAddress);
+        weth = WETH(wethAddress);
+        cEth = cETH(cEthAddress);
         aaveLendingPool = AaveLendingPool(aaveLendingPoolAddress);
     }
 
-    function deposit(uint256 _amount, uint256 _compAPY, uint256 _aaveAPY) public onlyOwner {
-        require(_amount > 0, "Invalid amount");
+    // Functions
 
+    function deposit(uint256 _amount, uint256 _compAPY, uint256 _aaveAPY) public onlyOwner {
+        require(_amount > 0);
+
+        // Rebalance in the case of a protocol with the higher rate after their initial deposit,
+        // is no longer the higher interest rate during this deposit...
         if (amountDeposited > 0) {
             rebalance(_compAPY, _aaveAPY);
         }
 
-        wrappedETH.transferFrom(msg.sender, address(this), _amount);
-        wrappedETH.deposit.value(_amount)();
+        // Deposit WETH
+        weth.transferFrom(msg.sender, address(this), _amount);
+        weth.deposit.value(_amount)();
 
         amountDeposited = amountDeposited.add(_amount);
 
+        // Compare interest rates
         if (_compAPY > _aaveAPY) {
-            compoundETH.mint.value(_amount)();
-            locationOfFunds = address(compoundETH);
+            // Deposit into Compound
+            cEth.mint.value(_amount)();
+            locationOfFunds = address(cEth);
         } else {
-            aaveLendingPool.deposit(address(wrappedETH), _amount, address(this), 0);
+            // Deposit into Aave
+            aaveLendingPool.deposit(address(weth), _amount, address(this), 0);
             locationOfFunds = address(aaveLendingPool);
         }
 
@@ -99,17 +113,19 @@ contract YieldAggregator {
     }
 
     function withdraw(uint256 _amount) public onlyOwner {
-        require(_amount > 0 && _amount <= amountDeposited, "Invalid amount");
+        require(_amount > 0 && _amount <= amountDeposited);
 
-        if (locationOfFunds == address(compoundETH)) {
-            uint256 compoundETHBalance = compoundETH.balanceOf(address(this));
-            uint256 redeemAmount = compoundETHBalance.mul(_amount).div(amountDeposited);
-            uint256 withdrawnAmount = compoundETH.redeem(redeemAmount);
+        if (locationOfFunds == address(cEth)) {
+            // Withdraw from Compound
+            uint256 cEthBalance = cEth.balanceOf(address(this));
+            uint256 redeemAmount = cEthBalance.mul(_amount).div(amountDeposited);
+            uint256 withdrawnAmount = cEth.redeem(redeemAmount);
             require(withdrawnAmount > 0, "Compound withdraw failed");
             require(address(this).balance >= withdrawnAmount, "Insufficient ETH balance");
             msg.sender.transfer(withdrawnAmount);
         } else {
-            aaveLendingPool.withdraw(address(wrappedETH), _amount, msg.sender);
+            // Withdraw from Aave
+            aaveLendingPool.withdraw(address(weth), _amount, msg.sender);
         }
 
         amountDeposited = amountDeposited.sub(_amount);
@@ -118,17 +134,18 @@ contract YieldAggregator {
     }
 
     function rebalance(uint256 _compAPY, uint256 _aaveAPY) internal {
-        if (locationOfFunds == address(compoundETH)) {
-            uint256 compoundETHBalance = compoundETH.balanceOf(address(this));
-            uint256 redeemAmount = compoundETHBalance.mul(amountDeposited).div(compoundETHBalance.add(amountDeposited));
-            uint256 redeemedAmount = compoundETH.redeem(redeemAmount);
+        if (locationOfFunds == address(cEth)) {
+            uint256 cEthBalance = cEth.balanceOf(address(this));
+            uint256 redeemAmount = cEthBalance.mul(amountDeposited).div(cEthBalance.add(_amount));
+            uint256 redeemedAmount = cEth.redeem(redeemAmount);
             require(redeemedAmount > 0, "Compound redeem failed");
             require(address(this).balance >= redeemedAmount, "Insufficient ETH balance");
-            wrappedETH.withdraw(redeemedAmount);
+            weth.withdraw(redeemedAmount);
         } else {
-            aaveLendingPool.withdraw(address(wrappedETH), amountDeposited, address(this));
+            aaveLendingPool.withdraw(address(weth), amountDeposited, address(this));
         }
     }
 
+    // Fallback function
     function() external payable {}
 }
